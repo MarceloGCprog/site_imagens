afaf0a7876290410cfb74d053d764cf65807b8fb
MarceloGCprog <auditorjamar@gmail.com>
2022-06-27 16:41:29 -0300

    adicao do semantic-ui e criacao do arquivo appheader.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <AppHeaderVue></AppHeaderVue>
+    </div>
+</template>
+
+<script>
+    import AppHeaderVue from './components/AppHeader.vue';
+    export default {
+        name: 'App',
+        components:{
+            AppHeaderVue
+        }
+    };
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a class=" active item" href="/">
+        Image Storage
+        </a>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name:'AppHeader'
+    }
+</script>
+
+<style>
+
+</style>
