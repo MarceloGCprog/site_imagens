251b9f344584d010e9e643fc2978079cbf6dfc2c
MarceloGCprog <auditorjamar@gmail.com>
2022-07-08 20:23:36 -0300

    adicao vue-router, criacao do AuthHandler, inicio mudanca AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,8 +3,21 @@
         <a class=" active item" href="/">
         Image Storage
         </a>
+        
         <div class="right menu">
-            <a href="#" class="ui item" @click="login">
+            <div class="row" v-if="isLoggedIn">
+            <a href="#" class="ui item" >
+            Galleries
+            </a>
+            <a href="#" class="ui item" >
+            Upload
+            </a>
+            <a href="#" class="ui item" >
+            Logout
+            </a>
+
+            </div>
+            <a v-else href="#" class="ui item" @click="login">
             Login
             </a>
         
@@ -16,10 +29,12 @@
 <script>
 
     import { mapActions } from 'vuex';
+    import { mapGetters } from 'vuex';
 
     export default {
         name:'AppHeader',
-        methods: mapActions(['login'])
+        methods: mapActions(['login']),
+        computed: mapGetters(['isLoggedIn'])
     }
 </script>
 
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur';
+import qs from 'qs';
 
 
 const state = {
@@ -23,12 +24,31 @@ const actions = {
         commit('setToken',null);
     },
 
-    /*login: function(){
+    login: function(){
         return api.login();
-    },*/
-    login: () => {
-        api.login();
-      }
+    },
+    finalizeLogin: function({commit}, hash){
+        let strutHash = qs.parse(hash.replace('#','')); //strutHash recebe todo o hash enviado pela pagina callback do auth
+                                                        //em seguida substitui o # por ' ' de modo a tirar o simbolo
+                                                        //por fim, a lib qs transforma o hash restante em uma estrutura
+                                                        // tipo chave valor.
+        /* EXEMPLO HASH: 
+        '#access_token=b117abc50573ecae122bbc3a767a276836b30bd2&expires_in=315360000&token_type=bearer&
+        refresh_token=6b45e13b0d88819e3d8e3c12c0e7bc70aa08f87f&account_username=myvueimagesite&
+        account_id=163416225'
+        observar a estrutura:
+        access_token =
+        expires_in=
+        token_type=
+        refresh_token=
+        account_username=
+        account_id=
+        Tudo separar por '&'
+        */
+        commit('setToken',strutHash.access_token);
+        
+    }
+    
 
 };
 
