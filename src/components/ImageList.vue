be95e75da2d2deebd417360206059bbbaafafdcb
MarceloGCprog <auditorjamar@gmail.com>
2022-07-19 22:15:23 -0300

    adicao de estilo e verificacao de log antes de mostrar imagens

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,16 +1,17 @@
 <template>
-    <div >
-        <div v-for="image in AllImages" v-bind:key="image.id">
-        <img class="ui middle aligned tiny image" v-bind:src="image.link"/>
-        <span>Id {{image.id}}</span>        
-        
-        </div>
-         
-        
+<div>
+    <div v-if="isLoggedIn" class="image-container">        
+        <img  v-for="image in AllImages" v-bind:key="image.id" v-bind:src="image.link"/> 
+
         <!-- cada 'image' eh um array com diversas informacoes que bem do backend
         a propriedade src faz reference ao link que a tag 'img' precisa renderizar
         jah a prop key eh obrigatoria e eh um meio de definir aquela info. como unica  -->
     </div>
+    <div v-else>
+        <h2> Log in to get Started!!!</h2>
+    </div>
+</div>
+    
 </template>
 
 <script>
@@ -19,7 +20,7 @@
 
     export default {
         name: 'ImageList',
-        computed: mapGetters(['AllImages']),
+        computed: mapGetters(['AllImages', 'isLoggedIn']),
         methods: mapActions(['FetchImages']),
         created(){ //funcao chamada na criacao da pagina. Tudo que estiver aqui sera renderizado
         
@@ -30,5 +31,13 @@
 </script>
 
 <style scoped>
+.image-container{
+    column-count: 3; /* Estamos criando 3 colunas para as imagens */
+    column-gap: 0px; /* As colunas se tocam (0px) */
+}
+img {
+    max-width: 100%;
+    padding: 5px; /* Dentro de cada coluna havera um espaco int de 5 px entre imagens */
+}
 
 </style>
